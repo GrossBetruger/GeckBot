52c42e89ac05a3831be24ec5f2c938c8fe437966
Oren Koriat <oren@cynerio.co>
2021-08-18 15:57:24 +0300

    single query get 1000 coins by market cap desc

--- python/gecko.py
+++ python/gecko.py
@@ -1,52 +1,70 @@
+from time import sleep
+
 import pandas as pd
 import requests
 
-from json import dump, load
-from random import shuffle
+from datetime import datetime
+from json import load
 from pprint import pprint
 from sheet_updater import update_sheet
-from datetime import datetime
+
+MAX_PAGE_SIZE = 250
 
 TOKEN_PRICE_URL = "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd"
 
-LIST_COINS_URL = "https://api.coingecko.com/api/v3/coins/list"
+LIST_COINS_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page={}&page={}&sparkline=false' "
 
 
-def query_coins() -> list[str]:
-    coins = requests.get(LIST_COINS_URL).json()
-    coins = [coin['name'] for coin in coins]
+def query_coins() -> list[dict]:
+    coins = []
+    for page in range(1, 5):
+        print(f'querying page #: {page} of size: {MAX_PAGE_SIZE}')
+        coins += requests.get(LIST_COINS_URL.format(MAX_PAGE_SIZE, page)).json()
     return coins
 
 
-def read_coins() -> list[str]:
+def read_coins() -> list[dict]:
     with open('coins.json') as f:
         return load(f)
 
 
 def get_prices(coin_param: str):
-    # print(TOKEN_PRICE_URL.format(coin_param))
-    # return
     resp = requests.get(TOKEN_PRICE_URL.format(coin_param))
     resp.raise_for_status()
     print(resp)
     return resp.json()
 
 
-def parser_prices(api_price_response: dict):
+def parser_prices(api_market_response: list[dict]) -> pd.DataFrame:
     rows = []
     last_updated = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    for k, v in api_price_response.items():
-        if k not in ['404']:
-            if 'usd' not in v:
+    columns = {'name', 'market_cap', 'current_price', 'market_cap_change_percentage_24h', 'fully_diluted_valuation'}
+    for coin_data in api_market_response:
+        row = dict()
+        for k, v in coin_data.items():
+            if k not in columns:
                 continue
-            rows.append([k, v['usd'], last_updated])
-    return pd.DataFrame(rows, columns=['token', 'price (usd)', 'last update'])
+            if v in [None]:
+                continue
+            row[k.replace('_', ' ')] = v
+        row['last updated'] = last_updated
+        rows.append(row)
+    return pd.DataFrame(rows).fillna('')
+
+
+def main():
+    coins = query_coins()  # read_coins()
+    pprint(coins)
+    table = parser_prices(coins)
+    print(table)
+    sheet_title = 'gecko'
+    print(f"Updating spreadsheet: '{sheet_title}'...")
+    update_sheet(sheet_title, table)
+    print("DONE")
 
 
 if __name__ == '__main__':
-    coins = read_coins()
-    shuffle(coins)
-    prices = get_prices(','.join(coins[:200]))
-    pprint(prices)
-    table = parser_prices(prices)
-    update_sheet('test sheet', table)
+    while True:
+        main()
+        print("sleeping...")
+        sleep(3600)
--- python/sheet_updater.py
+++ python/sheet_updater.py
@@ -1,11 +1,16 @@
 import os
+from typing import List
+
 import gspread
-
-from typing import Union, List
-from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from gspread_formatting import *
+from oauth2client.service_account import ServiceAccountCredentials
 
-
+COLUMNS_FORMATTING = CellFormat(
+    backgroundColor=Color.fromHex("#7733FF"),
+    textFormat=TextFormat(bold=True, foregroundColor=Color.fromHex("#FFFFFF")),
+    horizontalAlignment='CENTER'
+    )
 
 scope = ['https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive.file',
@@ -33,6 +38,7 @@ def update_sheet(spreadsheet_title: str, data: pd.DataFrame):
     global client
     data = [data.columns.values.tolist()] + data.values.tolist()
     worksheet = client.open(spreadsheet_title).sheet1
+    format_cell_range(worksheet, 'A1:F1', COLUMNS_FORMATTING)
     worksheet.update(data)
 
 
@@ -40,4 +46,3 @@ if __name__ == '__main__':
     data = pd.DataFrame([['data1', 'data2'], ['data3', 'data4']], columns=['col1', 'col2'])
     update_sheet("test sheet", data)
     print(read_sheet("test sheet"))
-
